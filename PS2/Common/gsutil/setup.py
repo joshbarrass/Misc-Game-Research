a0f962892369ea5aa4658b4f3e96cc30552466f5
Joshua Barrass <josh.barrass.work@gmail.com>
2024-02-20 23:39:38 +0000

    setup.py for building gsutil via distutils

---
+++ PS2/Common/gsutil/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+setup.py file for building gsutil
+
+Based on setup.py from SWIG documentation.
+https://swig.org/Doc4.0/Python.html#Python_nn6
+"""
+
+from distutils.core import setup, Extension
+
+
+example_module = Extension('_gsutil',
+                           sources=['gsreg.cpp', 'gsutil.cpp'],
+                           )
+
+setup (name = 'gsutil',
+       version = '0.1',
+       author      = "Murugo",
+       description = """Tool for simulating GS memory""",
+       ext_modules = [example_module],
+       py_modules = ["gsutil"],
+       )
+
