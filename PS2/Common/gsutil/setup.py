5b5af4cfb8a1269db9c5545f6ee8691de56014fd
Joshua Barrass <josh.barrass.work@gmail.com>
2024-02-21 00:21:45 +0000

    Download swigwin from sourceforge

--- PS2/Common/gsutil/setup.py
+++ PS2/Common/gsutil/setup.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 """
 setup.py file for building gsutil
 
@@ -9,16 +8,16 @@ https://swig.org/Doc4.0/Python.html#Python_nn6
 
 from distutils.core import setup, Extension
 
+example_module = Extension(
+    '_gsutil',
+    sources=['gsreg.cpp', 'gsutil.cpp', 'gsutil_wrap.cxx'],
+)
 
-example_module = Extension('_gsutil',
-                           sources=['gsreg.cpp', 'gsutil.cpp'],
-                           )
-
-setup (name = 'gsutil',
-       version = '0.1',
-       author      = "Murugo",
-       description = """Tool for simulating GS memory""",
-       ext_modules = [example_module],
-       py_modules = ["gsutil"],
-       )
-
+setup(
+    name='gsutil',
+    version='0.1',
+    author="Murugo",
+    description="""Tool for simulating GS memory""",
+    ext_modules=[example_module],
+    py_modules=["gsutil"],
+)
